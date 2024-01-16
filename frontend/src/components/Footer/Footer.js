ea19ba38cdca8e60be3f54a768494daa14686a74
dhruvermafz <vermadhruv09112002@gmail.com>
2024-01-16 14:59:09 +0530

    added frontedn and backend

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright-details">
+        <p className="footer-copyright">
+          &#169; 2020 copyright all right reserved
+          <br />
+          <span>Designed by Dhruv Verma</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
